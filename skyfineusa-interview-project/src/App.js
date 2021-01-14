28de6b5edc8b92472ca7f18c3716bfaa1bfd7b85
Casey Fronk <caseyfronk@hotmail.com>
2021-01-14 12:41:00 -0700

    Added Input bar from rainbow-components - howeer, didn't find out how to make it filter by state. Will research more.

--- skyfineusa-interview-project/src/App.js
+++ skyfineusa-interview-project/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { TableWithBrowserPagination, Column, Application, Input } from 'react-rainbow-components';
+import States from './States';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    
+    const americanStateData = States;
+    
+    const [americanStates, setAmericanStates] = React.useState(americanStateData);
+    
+    const containerStyles = {
+    maxWidth: 700,
+    };
+
+
+    return (<div className="App">
+            
+            <Input
+        id="input-component-1"
+        label="Search States"
+        placeholder="Search States"
+        style={containerStyles}
+        className="rainbow-m-vertical_x-large rainbow-p-horizontal_medium rainbow-m_auto"
+    />;
+            
+        <TableWithBrowserPagination
+            pageSize={10}
+            data={americanStates}
+            keyField="name"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <Column header="State" field="name" />
+            <Column header="Abbreviation" field="abbreviation" />
+            <Column header="Population" field="population" />
+
+        </TableWithBrowserPagination>
+          </div>
+)
 }
 
 export default App;
---
+++ skyfineusa-interview-project/src/States.js
@@ -0,0 +1,301 @@
+import React from 'react'
+
+export default function States() {
+    return ( [
+    {
+        "name": "Alabama",
+        "abbreviation": "AL",
+        "population": 25025
+    },
+    {
+        "name": "Alaska",
+        "abbreviation": "AK",
+        "population": 25025
+    },
+    {
+        "name": "American Samoa",
+        "abbreviation": "AS",
+        "population": 25025
+    },
+    {
+        "name": "Arizona",
+        "abbreviation": "AZ",
+        "population": 25025
+    },
+    {
+        "name": "Arkansas",
+        "abbreviation": "AR",
+        "population": 25025
+    },
+    {
+        "name": "California",
+        "abbreviation": "CA",
+        "population": 25025
+    },
+    {
+        "name": "Colorado",
+        "abbreviation": "CO",
+        "population": 25025
+    },
+    {
+        "name": "Connecticut",
+        "abbreviation": "CT",
+        "population": 25025
+    },
+    {
+        "name": "Delaware",
+        "abbreviation": "DE",
+        "population": 25025
+    },
+    {
+        "name": "District Of Columbia",
+        "abbreviation": "DC",
+        "population": 25025
+    },
+    {
+        "name": "Federated States Of Micronesia",
+        "abbreviation": "FM",
+        "population": 25025
+    },
+    {
+        "name": "Florida",
+        "abbreviation": "FL",
+        "population": 25025
+    },
+    {
+        "name": "Georgia",
+        "abbreviation": "GA",
+        "population": 25025
+    },
+    {
+        "name": "Guam",
+        "abbreviation": "GU",
+        "population": 25025
+    },
+    {
+        "name": "Hawaii",
+        "abbreviation": "HI",
+        "population": 25025
+    },
+    {
+        "name": "Idaho",
+        "abbreviation": "ID",
+        "population": 25025
+    },
+    {
+        "name": "Illinois",
+        "abbreviation": "IL",
+        "population": 25025
+    },
+    {
+        "name": "Indiana",
+        "abbreviation": "IN",
+        "population": 25025
+    },
+    {
+        "name": "Iowa",
+        "abbreviation": "IA",
+        "population": 25025
+    },
+    {
+        "name": "Kansas",
+        "abbreviation": "KS",
+        "population": 25025
+    },
+    {
+        "name": "Kentucky",
+        "abbreviation": "KY",
+        "population": 25025
+    },
+    {
+        "name": "Louisiana",
+        "abbreviation": "LA",
+        "population": 25025
+    },
+    {
+        "name": "Maine",
+        "abbreviation": "ME",
+        "population": 25025
+    },
+    {
+        "name": "Marshall Islands",
+        "abbreviation": "MH",
+        "population": 25025
+    },
+    {
+        "name": "Maryland",
+        "abbreviation": "MD",
+        "population": 25025
+    },
+    {
+        "name": "Massachusetts",
+        "abbreviation": "MA",
+        "population": 25025
+    },
+    {
+        "name": "Michigan",
+        "abbreviation": "MI",
+        "population": 25025
+    },
+    {
+        "name": "Minnesota",
+        "abbreviation": "MN",
+        "population": 25025
+    },
+    {
+        "name": "Mississippi",
+        "abbreviation": "MS",
+        "population": 25025
+    },
+    {
+        "name": "Missouri",
+        "abbreviation": "MO",
+        "population": 25025
+    },
+    {
+        "name": "Montana",
+        "abbreviation": "MT",
+        "population": 25025
+    },
+    {
+        "name": "Nebraska",
+        "abbreviation": "NE",
+        "population": 25025
+    },
+    {
+        "name": "Nevada",
+        "abbreviation": "NV",
+        "population": 25025
+    },
+    {
+        "name": "New Hampshire",
+        "abbreviation": "NH",
+        "population": 25025
+    },
+    {
+        "name": "New Jersey",
+        "abbreviation": "NJ",
+        "population": 25025
+    },
+    {
+        "name": "New Mexico",
+        "abbreviation": "NM",
+        "population": 25025
+    },
+    {
+        "name": "New York",
+        "abbreviation": "NY",
+        "population": 25025
+    },
+    {
+        "name": "North Carolina",
+        "abbreviation": "NC",
+        "population": 25025
+    },
+    {
+        "name": "North Dakota",
+        "abbreviation": "ND",
+        "population": 25025
+    },
+    {
+        "name": "Northern Mariana Islands",
+        "abbreviation": "MP",
+        "population": 25025
+    },
+    {
+        "name": "Ohio",
+        "abbreviation": "OH",
+        "population": 25025
+    },
+    {
+        "name": "Oklahoma",
+        "abbreviation": "OK",
+        "population": 25025
+    },
+    {
+        "name": "Oregon",
+        "abbreviation": "OR",
+        "population": 25025
+    },
+    {
+        "name": "Palau",
+        "abbreviation": "PW",
+        "population": 25025
+    },
+    {
+        "name": "Pennsylvania",
+        "abbreviation": "PA",
+        "population": 25025
+    },
+    {
+        "name": "Puerto Rico",
+        "abbreviation": "PR",
+        "population": 25025
+    },
+    {
+        "name": "Rhode Island",
+        "abbreviation": "RI",
+        "population": 25025
+    },
+    {
+        "name": "South Carolina",
+        "abbreviation": "SC",
+        "population": 25025
+    },
+    {
+        "name": "South Dakota",
+        "abbreviation": "SD",
+        "population": 25025
+    },
+    {
+        "name": "Tennessee",
+        "abbreviation": "TN",
+        "population": 25025
+    },
+    {
+        "name": "Texas",
+        "abbreviation": "TX",
+        "population": 25025
+    },
+    {
+        "name": "Utah",
+        "abbreviation": "UT",
+        "population": 25025
+    },
+    {
+        "name": "Vermont",
+        "abbreviation": "VT",
+        "population": 25025
+    },
+    {
+        "name": "Virgin Islands",
+        "abbreviation": "VI",
+        "population": 25025
+    },
+    {
+        "name": "Virginia",
+        "abbreviation": "VA",
+        "population": 25025
+    },
+    {
+        "name": "Washington",
+        "abbreviation": "WA",
+        "population": 25025
+    },
+    {
+        "name": "West Virginia",
+        "abbreviation": "WV",
+        "population": 25025
+    },
+    {
+        "name": "Wisconsin",
+        "abbreviation": "WI",
+        "population": 25025
+    },
+    {
+        "name": "Wyoming",
+        "abbreviation": "WY",
+        "population": 25025
+    }
+])
+}
